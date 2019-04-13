eb31abb73b8d6b6d5658beff421cfcf4f0a94d66
Cristian Steib <cristiansteib@gmail.com>
2019-04-12 21:53:11 -0300

    starting point

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='cloudflare-dynamic-ip',
+    version='0.7.4',
+    author='Cristian Steib',
+    author_email='cristiansteib@gmail.com',
+    description='Service to auto update ip in cloudflare',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='',
+    package_data={'resources': ['*', '**/*', '**/**/*']},
+    install_requires=[],
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'License :: Other/Proprietary License',
+    ],
+)
