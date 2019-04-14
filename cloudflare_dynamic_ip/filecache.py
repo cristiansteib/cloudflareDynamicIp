a97bec17fd5781745cb3cec3c84ddc4b8b48aff8
Cristian Steib <cristiansteib@gmail.com>
2019-04-14 14:20:49 -0300

    update

---
+++ cloudflare_dynamic_ip/filecache.py
@@ -0,0 +1,34 @@
+
+class FileCache:
+
+    def __init__(self, path=''):
+        self._directory = path
+        self._data = dict()
+
+    def _save_value_on_disk(self, key, value):
+        file = open(self._directory + key, mode='w')
+        file.write(value)
+        file.close()
+
+    def _retrieve_value_from_disk(self, key):
+        value = None
+        try:
+            file = open(self._directory + key, mode='r')
+            value = file.read()
+            file.close()
+        except FileNotFoundError:
+            pass
+        return value
+
+    def set(self, key, value):
+        old_value = self._data.get(key, None)
+        if old_value is not None and old_value != value:
+            self._data[key] = str(value).strip()
+            self._save_value_on_disk(key, self._data[key])
+
+    def get(self, key):
+        value = self._data.get(key, None)
+        if value is not None:
+            return value
+        self._data[key] = self._retrieve_value_from_disk(key)
+        return self._data[key]
--- cloudflare_dynamic_ip/service.py
+++ cloudflare_dynamic_ip/service.py
@@ -2,11 +2,15 @@ from pathlib import Path
 import logging
 import time
 import json
-
+import os
 from cloudflareapi import cloudflare
 from config import ConfigReader
+from filecache import FileCache
 import args
 import http.client
+
+FILE_TO_STORAGE_IP = 'IP'
+fileCache = FileCache()  # Cache the ip in the ram to avoid IO
 
 
 def get_public_ip():
@@ -22,10 +26,11 @@ def set_ip(ip, auth_key: str, auth_email: str, dns_type, urls: list):
     for url in urls:
         x = easy.update_dns_ip(dns_name=url, newIP=ip, dns_type=dns_type)
         try:
-            state = 'success' if x['value'].json()['result']['content'] == ip else 'FAILED'
-            logging.info(state + ' change for ' + url)
+            state = 'Success' if x['value'].json()['result']['content'] == ip else 'FAILED'
+            logging.info(state + ' change for record "{0}" on {1}'.format(dns_type, url))
         except:
-            logging.critical('FAIL change for ' + url + ' using token ' + auth_key)
+            logging.critical(
+                'FAILED to change IP for record "{0}" on {1} using token {2}'.format(dns_type, url, auth_key))
 
 
 def set_ip_dry_run(ip, auth_key: str, auth_email: str, dns_type, urls: list):
@@ -34,24 +39,24 @@ def set_ip_dry_run(ip, auth_key: str, auth_email: str, dns_type, urls: list):
 
 
 def get_last_ip():
-    pass
+    return fileCache.get('IP')
 
 
-def _save_new_ip(ip):
-    pass
+def save_new_ip(ip):
+    fileCache.set('IP', ip)
 
 
 def run(config, dry_run=False):
     current_ip = get_public_ip()
-    if current_ip is not get_last_ip():
-        logging.info("Ip change " + current_ip)
+    if current_ip != get_last_ip():
+        logging.info("Ip change to " + current_ip)
         all_hosts_data = config.get_token_with_all_hosts()
         for auth_email, auth_key, dns_type, hosts in all_hosts_data:
             if not dry_run:
                 set_ip(current_ip, auth_key, auth_email, dns_type, hosts)
             else:
                 set_ip_dry_run(current_ip, auth_key, auth_email, dns_type, hosts)
-        _save_new_ip(current_ip)
+        save_new_ip(current_ip)
 
 
 if __name__ == "__main__":
@@ -62,11 +67,10 @@ if __name__ == "__main__":
         log_dir = str(Path(args.log_directory) / 'cloudflare_dynamic_ip.log')
         logging.basicConfig(
             filename=log_dir,
-            format='%(asctime)s - %(message)s', level=logging.INFO)
+            format='[%(levelname)s] %(asctime)s - %(message)s', level=logging.INFO, datefmt='%d-%m-%y %H:%M:%S')
 
     while True:
         run(config, dry_run=args.dry_run)
         if not args.demonize:
             break
         time.sleep(10)
-
